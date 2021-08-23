4e801d26f3f7fee3aafced9d5847c979a207b5ae
Nikita <nikivanov.037@gmail.com>
2021-08-23 18:49:09 +0300

    вотемарка в демотиваторе

--- PhotoEditor/demotivator.py
+++ PhotoEditor/demotivator.py
@@ -9,6 +9,6 @@ def demotivator_create(file_name, text1, text2 = ''):
     new_name = ''.join(random.choice(letters) for i in range(12)) + '.jpg'
     dem = Demotivator(text1, text2)
     image_path = 'PhotoEditor/sent_photos/' + file_name
-    dem.create(image_path, RESULT_FILENAME=new_name)
+    dem.create(image_path, RESULT_FILENAME=new_name, line='tg: @memochelbot')
     return new_name
 
