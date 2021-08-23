8fae5ded7d8877e12f55c753508d97af8392cbb5
Nikita <nikivanov.037@gmail.com>
2021-08-23 18:34:45 +0300

    перенос фраз на дб

--- data/config.py
+++ data/config.py
@@ -2,13 +2,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = '1649758549:AAGLYak7wIuwb2w1c9j_d8f_XWJyHmAN_Cg'
+BOT_TOKEN = ''
 admins = [
-742184966
 ]
 
 PG_USER = 'postgres'
-PG_PASS = 'Q7m9kAcE2'
+PG_PASS = ''
 ip = 'localhost'
 
 
