4d0ba5505d927e8f6c156d389ea3141a82375d5a
Nikita <nikivanov.037@gmail.com>
2021-08-23 12:37:41 +0300

    peewee fix

--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -2,10 +2,10 @@ from .bot_info import dp
 from .commands import dp
 from .create_mem import dp
 from .donate import dp
-from .echo import dp
 from .help import dp
 from .photo_operations import dp
 from .plus_word import dp
 from .start import dp
 
+from .echo import dp
 __all__ = ["dp"]
