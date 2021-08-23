67087f6822cc0559e37afd36070d4fd0f786e17b
Nikita <nikivanov.037@gmail.com>
2021-08-23 12:26:47 +0300

    peewee fix

--- data/peewee.py
+++ data/peewee.py
@@ -1,21 +1,23 @@
 from peewee import *
 from data import config
 import psycopg2
+
 db = PostgresqlDatabase(
     'humans',
     port=5432,
     user=config.PG_USER,
     password=config.PG_PASS,
-    host=config.ip)
+    host=config.ip
+)
 
 
 class Human(Model):
-    id_primary= PrimaryKeyField(primary_key=1)
+    id_primary = PrimaryKeyField(primary_key=1)
     id = IntegerField()
     username = CharField(null=True)
     name = CharField(default='none')
     age = IntegerField(null=True)
     mode = CharField(default='Шакализатор')
-
+    void = IntegerField(null=True)
     class Meta:
         database = db
