b1925c5b6ba5fef0b7f695e7199b19b6737d307b
Nikita <nikivanov.037@gmail.com>
2021-08-26 13:06:20 +0300

    remove age

--- states/start_test.py
+++ states/start_test.py
@@ -25,24 +25,6 @@ async def name_save(message: types.Message):
     await StartTest.age.set()
 
 
-@dp.message_handler(state=StartTest.age)
-async def age_save(message: types.Message, state=FSMContext):
-    age = message.text
-    human = Human.get(id=message.from_user.id)
-
-    if age.isdigit() is True:
-        human.age = age
-        human.save()
-        await message.answer(text=f'Благодарю за искренность, {human.name}! '
-                                  f'Тебе наверное уже не терпится создать свой первый мэм?',
-                             reply_markup=menu_kb)
-        await state.finish()
-
-    else:
-        await message.answer('Похоже, ты прислал символы или число, содержащее символы. Напиши-ка ещё раз свой возраст')
-        await StartTest.age.set()
-
-
 @dp.message_handler(state=StartTest.name_change)
 async def name_change_final(message: types.Message, state: FSMContext):
     name = message.text
